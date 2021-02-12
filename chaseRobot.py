0af32780a3221f976d2f849e482695776935d637
Slawomir Chyla <chylaslawek@gmail.com>
2021-02-12 15:56:21 +0100

    Code cleanup and fixes

--- chaseRobot.py
+++ chaseRobot.py
@@ -11,26 +11,27 @@ UDP_PORT = 5050
 NUMBER_OF_VOICES = 2
 MAX_SPEED = 70
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
-
 chaseVoice = ["chase1.mp3", "chase2.mp3"]
 policeSiren = "police.mp3"
 
 class ChaseRobot:
     def __init__(self):
+        GPIO.setmode(GPIO.BCM)
         pygame.init()
         pygame.mixer.init()
         self.blinkLeds = -1;
         self.pill2kill = None
         self.voiceId = 0
         self.speed = MAX_SPEED 
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((UDP_IP, UDP_PORT))
+        self.task = None
 
 
-    def startup():
+    def startup(self):
         self.right()
         sleep(0.2)
-        slef.left()
+        self.left()
         sleep(0.4)
         self.right()
         sleep(0.2)
@@ -39,34 +40,34 @@ class ChaseRobot:
         pygame.mixer.music.play()
 
 
-    def forward():
+    def forward(self):
         motors.motor1.setSpeed(self.speed)
         motors.motor2.setSpeed(-self.speed)
 
 
-    def reverse():
+    def reverse(self):
         motors.motor1.setSpeed(-self.speed)
         motors.motor2.setSpeed(self.speed)
 
 
-    def left():
+    def left(self):
         motors.motor1.setSpeed(self.speed)
         motors.motor2.setSpeed(self.speed)
 
 
-    def right():
+    def right(self):
         motors.motor1.setSpeed(-self.speed)
         motors.motor2.setSpeed(-self.speed)
 
 
-    def stop():
+    def stop(self):
         motors.motor1.setSpeed(0)
+        motors.motor2.setSpeed(0)
 
 
-    def ioio(stop_event, arg):
+    def ioio(self, stop_event, arg):
         blueLed = 20
         redLed = 21
-        GPIO.setmode(GPIO.BCM)
         GPIO.setup(redLed, GPIO.OUT)
         GPIO.setup(blueLed, GPIO.OUT)
         while not stop_event.wait(1):
@@ -80,31 +81,32 @@ class ChaseRobot:
         GPIO.output(redLed, GPIO.LOW)
 
 
-    def action1():
+    def stopTask(self):
+        self.pill2kill.set()
+        self.task.join()
+        self.pill2kill = None
+        pygame.mixer.music.stop()
+
+
+    def action1(self):
         self.blinkLeds = ~self.blinkLeds
         if self.blinkLeds == -1:
-           self.pill2kill.set()
-           t.join()
-           pill2kill = None
-           pygame.mixer.music.stop()
+            self.stopTask()
 
 
-    def action2():
+    def action2(self):
         if self.blinkLeds == 0:
             self.blinkLeds = ~self.blinkLeds
-            self.pill2kill.set()
-            t.join()
-            self.pill2kill = None
-            pygame.mixer.music.stop()
-            pygame.mixer.music.load(chaseVoice[voiceId])
-            self.voiceId = (self.voiceId + 1) % NUMBER_OF_VOICES
-            pygame.mixer.music.play()
+            self.stopTask()
+        pygame.mixer.music.load(chaseVoice[self.voiceId])
+        self.voiceId = (self.voiceId + 1) % NUMBER_OF_VOICES
+        pygame.mixer.music.play()
 
 
-    def run():
+    def run(self):
         try:
           while True:
-              data, addr = sock.recvfrom(1024)
+              data, addr = self.sock.recvfrom(1024)
               raw = data
           
               if raw == "forward":
@@ -124,18 +126,19 @@ class ChaseRobot:
               else:
                   self.stop()
 
-              if self.blinkLeds == 0 and slef.pill2kill == None:
+              if self.blinkLeds == 0 and self.pill2kill == None:
                   self.pill2kill = threading.Event()
-                  t = threading.Thread(target=ioio, args=(pill2kill, "task"))
+                  self.task = threading.Thread(target=self.ioio, args=(self.pill2kill, "task"))
                   pygame.mixer.music.load(policeSiren)
                   pygame.mixer.music.play()
-                  t.start()
+                  self.task.start()
               
-          finally:
-              motors.setSpeeds(0, 0)
-              GPIO.cleanup()
+        finally:
+            motors.setSpeeds(0, 0)
+            GPIO.cleanup()
 
 def main():
+    GPIO.cleanup()
     chase = ChaseRobot()
     chase.startup()
     chase.run()
